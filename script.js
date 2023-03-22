13fdde3e9c5684144a5fd1dec8a2ac6ec8119ef1
Arturo Puentes <arturo.puentes.yu@gmail.com>
2023-03-22 23:03:54 +0100

    First commit

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 'use strict';
-window.onload = () => {};
+window.onload = () => {
+  const secretNum = Math.trunc(Math.random() * 20) + 1;
+  let score = 20;
+  const message = document.querySelector('.message');
+  const insertedNum = document.querySelector('.guess');
+  const playerScore = document.querySelector('.score');
+
+  document.querySelector('.check').addEventListener('click', function () {
+    const guessNum = Number(insertedNum.value);
+
+    if (!guessNum) {
+      message.textContent = 'No number!!';
+    } else if (guessNum === secretNum) {
+      message.textContent = 'Correct Number!';
+    } else if (guessNum > secretNum) {
+      message.textContent = 'Too high!';
+      if (score > 1) {
+        score--;
+        playerScore.textContent = score;
+      } else {
+        message.textContent = 'You lost the game';
+      }
+    } else if (guessNum < secretNum) {
+      message.textContent = 'Too low!';
+      if (score < 1) {
+        score--;
+        playerScore.textContent = score;
+      } else {
+        message.textContent = 'You lost the game';
+      }
+    }
+  });
+};
