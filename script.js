0c1efc416e419551c828e3fd931631711301cced
Arturo Puentes <arturo.puentes.yu@gmail.com>
2023-03-23 18:07:29 +0100

    Last Commit

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 'use strict';
 window.onload = () => {
-  const secretNum = Math.trunc(Math.random() * 20) + 1;
+  let secretNum = Math.trunc(Math.random() * 20) + 1;
   let score = 20;
   const message = document.querySelector('.message');
   const insertedNum = document.querySelector('.guess');
   const playerScore = document.querySelector('.score');
+  const body = document.querySelector('body');
+  const displayedNum = document.querySelector('.number');
+  let highScore = 0;
+
+  const changeMessage = message => {
+    message.textContent = message;
+  };
 
   document.querySelector('.check').addEventListener('click', function () {
     const guessNum = Number(insertedNum.value);
-
     if (!guessNum) {
-      message.textContent = 'No number!!';
+      changeMessage('No number!!');
     } else if (guessNum === secretNum) {
-      message.textContent = 'Correct Number!';
-    } else if (guessNum > secretNum) {
-      message.textContent = 'Too high!';
+      changeMessage('Correct Number!');
+      body.style.backgroundColor = '#60b347';
+      displayedNum.style.width = '30rem';
+      displayedNum.textContent = secretNum;
+
+      if (highScore < score) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+      }
+    } else if (guessNum !== secretNum) {
+      changeMessage(guessNum > secretNum ? 'Too high!' : 'Too low!');
       if (score > 1) {
         score--;
         playerScore.textContent = score;
       } else {
         message.textContent = 'You lost the game';
-      }
-    } else if (guessNum < secretNum) {
-      message.textContent = 'Too low!';
-      if (score < 1) {
-        score--;
-        playerScore.textContent = score;
-      } else {
-        message.textContent = 'You lost the game';
+        playerScore.textContent = 0;
       }
     }
+  });
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNum = Math.trunc(Math.random() * 20) + 1;
+    insertedNum.value = '';
+    playerScore.textContent = score;
+    displayedNum.textContent = '?';
+    displayedNum.style.width = '15rem';
+    changeMessage('Start guessing...');
+    body.style.backgroundColor = '#222';
   });
 };
